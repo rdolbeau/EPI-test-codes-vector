b0f767f475bcbf13b97323e49d822480ab6034c9
Romain Dolbeau <romain@dolbeau.org>
2020-04-27 11:48:10 +0200

    licenses

--- simplefft16-rvv/vf64.h
+++ simplefft16-rvv/vf64.h
@@ -1,3 +1,23 @@
+/*
+
+RISC-V V DFT/FFT demo code.
+
+Copyright ? 2020 Romain Dolbeau <Romain.Dolbeau@sipearl.com>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+version 2 as published by the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 //ZIP1 Interleave elements from low halves of two vectors
 static inline __epi_1xf64 __builtin_epi_zip1_1xf64(__epi_1xf64 v0, __epi_1xf64 v1, unsigned long int vc) {
 	__epi_1xf64 res;
